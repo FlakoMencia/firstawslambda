9bc14460e4b54e9a7d1fe5421b4961ca408ceda3
Flako Mencia <mario_mencia@hotmail.com>
2023-08-13 23:47:17 -0600

    init commit

---
+++ src/main/java/dev/flako/firstawslambda/FirstawslambdaApplication.java
@@ -0,0 +1,13 @@
+package dev.flako.firstawslambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstawslambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstawslambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/flako/firstawslambda/FirstawslambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.flako.firstawslambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstawslambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
